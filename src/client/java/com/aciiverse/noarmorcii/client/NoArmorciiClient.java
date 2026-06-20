ca0c997ca5986330e03aa3763cdfd5c80cc6f783
Florian Schulz <florian.schulz404@gmail.com>
2026-06-20 09:47:47 +0200

    🎉 start project

---
+++ src/client/java/com/aciiverse/noarmorcii/client/NoArmorciiClient.java
@@ -0,0 +1,11 @@
+package com.aciiverse.noarmorcii.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoArmorciiClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
